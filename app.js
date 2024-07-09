2974c95ee36964aa70b2727eab6acf4f02e5230c
KC Lim <kclim0528@gmail.com>
2024-07-09 08:31:05 -0700

    added reviews and login page

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const port = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use(expressLayouts);
+app.set('layout', './layouts/main'); // Ensure the layout path is correct
+app.set('view engine', 'ejs');
+
+
+//routes
+app.use('/',require('./server/routes/index'))
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
---
+++ server/controllers/mainController.js
@@ -0,0 +1,28 @@
+exports.homepage = async (req,res)=>{
+    try{
+        const locals = {
+            title : "notes app",
+            description:"a full stack notes app"
+        }
+        res.render('index',locals)
+    }catch(error){
+        res.status(500).send('error while loading homepage')
+    }
+}
+
+
+exports.about = async (req,res) =>{
+    const locals = {
+        title: "About- Notes app ",
+        description:"notes app that's hosted on AWS "
+    }
+    res.render('about',locals)
+}
+
+exports.reviews = async(req,res)=>{
+    try{
+        res.status(200).render('reviews')
+    }catch(error){
+        res.status(500).send("error loading features")
+    }
+}
