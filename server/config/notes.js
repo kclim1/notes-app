d16672e3ddd476186884ae6d6d10477515bc8db7
KC Lim <kclim0528@gmail.com>
2024-08-29 00:28:53 -0700

    removed useless variables

--- server/config/notes.js
+++ server/config/notes.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-const path = require('path')
-const notes = require(path.join(__dirname, '..', 'config', 'notes'));
 
 
 const notesSchema = new mongoose.Schema({
